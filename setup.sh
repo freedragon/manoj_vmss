0935b3bfdfb058bc2d6bdca99b9cc850f12c2e7a
bedro96@naver.com <bedro96@naver.com>
2019-11-12 12:53:33 +0000

    changing setup.sh

--- setup.sh
+++ setup.sh
@@ -12,11 +12,15 @@ apt update
 apt -y install python3-pip
 pip3 install psutil bottle configparser
 
+#force to use python3 as default
+ln -s /usr/bin/python3 /usr/bin/python -f
+
 #checkout code
 git clone https://github.com/manojsingh/azvmscripts.git
 
 #Start health probe job
 cd azvmscripts
+#make py file executable
 chmod +x *.py
 nohup ./health_probe_handler.py & echo $! > health-probe-pid.file &
 
